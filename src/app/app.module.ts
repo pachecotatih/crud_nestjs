2c3dce18bc929e617f6c203d5b5ce2e624ba2486
Tatiana Pacheco Barreto <tatianapacheco09@gmail.com>
2026-05-18 13:10:48 -0300

    namespace

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,36 +4,32 @@ import { AppService } from './app.service';
 import { RecadosModule } from 'src/recados/recados.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PessoasModule } from 'src/pessoas/pessoas.module';
-import { ConfigModule } from '@nestjs/config';
-import * as Joi from '@hapi/joi';
+import { ConfigModule, ConfigService, ConfigType } from '@nestjs/config';
+import appConfig from './app.config';
+
 // TypeOrmModule.forRoot() -> para raiz da aplicação
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule.forFeature(appConfig)],
+      inject: [appConfig.KEY],
+      useFactory: async (appConfigurations: ConfigType<typeof appConfig>) => {
+        console.log('TypeOrmModule', appConfigurations.environment);
+        return {
+          type: appConfigurations.database.type,
+          host: appConfigurations.database.host,
+          port: appConfigurations.database.port,
+          username: appConfigurations.database.username,
+          password: appConfigurations.database.password,
+          database: appConfigurations.database.database,
+          autoLoadEntities: appConfigurations.database.autoLoadEntities, // carrega automaticamente as entidades do projeto sem especificá-las
+          synchronize: appConfigurations.database.synchronize, // Sincroniza com o banco de dados. Não deve ser usado em produção
+        };
+      },
+    }),
     RecadosModule,
     PessoasModule,
-    ConfigModule.forRoot({
-      //envFilePath: '.env',
-      validationSchema: Joi.object({
-        DATABASE_TYPE: Joi.string().required(),
-        DATABASE_HOST: Joi.string().required(),
-        DATABASE_PORT: Joi.number().default(5432),
-        DATABASE_USERNAME: Joi.string().required(),
-        DATABASE_PASSWORD: Joi.string().required(),
-        DATABASE_DATABASE: Joi.string().required(),
-        DATABASE_AUTOLOADENTITIES: Joi.number().min(0).max(1).default(0),
-        DATABASE_SYNCHRONIZE: Joi.number().min(0).max(1).default(0),
-      }),
-    }),
-    TypeOrmModule.forRoot({
-      type: process.env.DATABASE_TYPE as 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: Number(process.env.DATABASE_PORT),
-      username: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_DATABASE,
-      autoLoadEntities: Boolean(process.env.DATABASE_AUTOLOADENTITIES), // carrega automaticamente as entidades do projeto sem especificá-las
-      synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE), // Sincroniza com o banco de dados. Não deve ser usado em produção
-    }),
   ],
   controllers: [AppController],
   providers: [AppService],
