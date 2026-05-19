47ac08ce3d1efd7ca5579c23284d3c2dd8a8f7cb
Tatiana Pacheco Barreto <tatianapacheco09@gmail.com>
2026-05-19 15:58:42 -0300

    criar várias imagens

--- src/pessoas/pessoas.controller.ts
+++ src/pessoas/pessoas.controller.ts
@@ -9,6 +9,7 @@ import {
   UseGuards,
   UseInterceptors,
   UploadedFile,
+  UploadedFiles,
 } from '@nestjs/common';
 import { PessoasService } from './pessoas.service';
 import { CreatePessoaDto } from './dto/create-pessoa.dto';
@@ -16,7 +17,7 @@ import { UpdatePessoaDto } from './dto/update-pessoa.dto';
 import { AuthTokenGuard } from 'src/auth/guards/auth-token.guard';
 import { TokenPayloadParam } from 'src/auth/params/token-payload.param';
 import { TokenPayloadDto } from 'src/auth/dto/token-payload.dto';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import * as path from 'path';
 import * as fs from 'fs/promises'; //filesystem
 import { randomUUID } from 'crypto';
@@ -61,25 +62,23 @@ export class PessoasController {
   }
 
   @UseGuards(AuthTokenGuard)
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(FilesInterceptor('file'))
   @Post('upload-picture')
   async uploadPicture(
-    @UploadedFile() file: Express.Multer.File,
+    @UploadedFiles() files: Array<Express.Multer.File>,
     @TokenPayloadParam() tokenPayload: TokenPayloadDto,
   ) {
-    const fileExtension = path
-      .extname(file.originalname)
-      .toLocaleLowerCase()
-      .substring(1);
-    const fileName = `${tokenPayload.sub}.${fileExtension}`;
-    const fileFullPath = path.resolve(process.cwd(), 'pictures', fileName);
-    await fs.writeFile(fileFullPath, file.buffer);
-    return {
-      fieldname: file.fieldname,
-      originalname: file.originalname,
-      mimetype: file.mimetype,
-      buffer: {},
-      size: file.size,
-    };
+    const result: string[] = [];
+    files.forEach(async file => {
+      const fileExtension = path
+        .extname(file.originalname)
+        .toLocaleLowerCase()
+        .substring(1);
+      const fileName = `${randomUUID()}.${fileExtension}`;
+      const fileFullPath = path.resolve(process.cwd(), 'pictures', fileName);
+      result.push(fileFullPath);
+      await fs.writeFile(fileFullPath, file.buffer);
+    });
+    return result;
   }
 }
