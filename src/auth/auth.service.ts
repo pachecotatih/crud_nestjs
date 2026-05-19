5aa8491bf2348b778776d88cdf88488a878f4a92
Tatiana Pacheco Barreto <tatianapacheco09@gmail.com>
2026-05-19 12:25:39 -0300

    refresh token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,9 @@
-import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  Inject,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
 import { Repository } from 'typeorm';
 import { Pessoa } from 'src/pessoas/entities/pessoa.entity';
@@ -7,6 +12,7 @@ import { HashingServiceProtocol } from './hashing/hashing.service';
 import jwtConfig from './config/jwt.config';
 import type { ConfigType } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
+import { RefreshTokenDto } from './dto/refresh-token.dto';
 @Injectable()
 export class AuthService {
   constructor(
@@ -32,16 +38,58 @@ export class AuthService {
       throw new UnauthorizedException('Usuário ou senha inválidos');
     }
 
-    const accessToken = await this.jwtService.signAsync({
-      sub: pessoa!.id,
-      email: pessoa!.email,
-    },{
+    return this.createTokens(pessoa!);
+  }
+
+  private async createTokens(pessoa: Pessoa) {
+    const accessTokenPromise = this.signJwtAsync<Partial<Pessoa>>(
+      pessoa!.id,
+      this.jwtConfiguration.jwtTtl,
+      { email: pessoa!.email },
+    );
+    const refreshTokenPromise = this.signJwtAsync<Partial<Pessoa>>(
+      pessoa!.id,
+      this.jwtConfiguration.jwtRefreshTtl,
+      { email: pessoa!.email },
+    );
+    const [accessToken, refreshToken] = await Promise.all([
+      accessTokenPromise,
+      refreshTokenPromise,
+    ]);
+
+    return { message: 'Login bem-sucedido', accessToken, refreshToken };
+  }
+
+  private async signJwtAsync<T>(sub: number, expiresIn: number, payload: T) {
+    return await this.jwtService.signAsync(
+      {
+        sub,
+        ...payload,
+      },
+      {
         audience: this.jwtConfiguration.audience,
         issuer: this.jwtConfiguration.issuer,
         secret: this.jwtConfiguration.secret,
-        expiresIn: this.jwtConfiguration.jwtTtl,
-    });
+        expiresIn: expiresIn,
+      },
+    );
+  }
 
-    return { message: 'Login bem-sucedido', accessToken };
+  async refreshTokens(refreshTokenDto: RefreshTokenDto) {
+    try {
+      const { sub } = await this.jwtService.verifyAsync(
+        refreshTokenDto.refreshToken,
+        this.jwtConfiguration,
+      );
+      const pessoa = await this.pessoaRepository.findOneBy({ id: sub });
+      if (!pessoa) {
+        throw new NotFoundException('Pessoa não encontrada');
+      }
+      return this.createTokens(pessoa);
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new UnauthorizedException(error.message);
+      } else throw new UnauthorizedException('Refresh Token inválido!');
+    }
   }
 }
