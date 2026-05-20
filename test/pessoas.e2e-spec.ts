072cbe549ff11ebed335f0f5ec8b3ffbb62a7593
Tatiana Pacheco Barreto <tatianapacheco09@gmail.com>
2026-05-20 16:55:19 -0300

    teste com jwt

--- test/pessoas.e2e-spec.ts
+++ test/pessoas.e2e-spec.ts
@@ -1,8 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types';
-import { AppModule } from '../src/app/app.module';
 import { RecadosModule } from 'src/recados/recados.module';
 import { PessoasModule } from 'src/pessoas/pessoas.module';
 import { GlobalConfigModule } from 'src/global-config/global-config.module';
@@ -12,12 +11,37 @@ import globalConfig from 'src/global-config/global-config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import * as path from 'path';
-import { ParseIntIdPipe } from 'src/common/pipes/parse-int-id.pipe';
 import appConfig from 'src/app/config/app.config';
-import { create } from 'domain';
+
+const login = async (
+  app: INestApplication,
+  email: string,
+  password: string,
+) => {
+  const response = await request(app.getHttpServer())
+    .post('/auth')
+    .send({ email, password });
+
+  return response.body.accessToken;
+};
+
+const createUserAndLogin = async (app: INestApplication) => {
+  const nome = 'Any User';
+  const email = 'anyuser@email.com';
+  const password = '123456';
+
+  await request(app.getHttpServer()).post('/pessoas').send({
+    nome,
+    email,
+    password,
+  });
+
+  return login(app, email, password);
+};
 
 describe('PessoasController (e2e)', () => {
   let app: INestApplication<App>;
+  let authToken: string;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -48,6 +72,7 @@ describe('PessoasController (e2e)', () => {
     app = module.createNestApplication();
     appConfig(app);
     await app.init();
+    authToken = await createUserAndLogin(app);
   });
 
   describe('/pessoas (POST)', () => {
@@ -140,6 +165,207 @@ describe('PessoasController (e2e)', () => {
       expect(response.body.message).toContain(
         'password must be longer than or equal to 5 characters',
       );
+    });
+
+    it('deve falhar se o nome for vazio', async () => {
+      const createPessoaDto = {
+        nome: '',
+        email: 'tatiana1@email.com',
+        password: '123456',
+      };
+
+      const response = await request(app.getHttpServer())
+        .post('/pessoas')
+        .send(createPessoaDto)
+        .expect(HttpStatus.BAD_REQUEST);
+      expect(response.body.message).toContain(
+        'nome must be longer than or equal to 3 characters',
+      );
+      expect(response.body.message).toContain('nome should not be empty');
+    });
+
+    it('deve falhar se o email for vazio', async () => {
+      const createPessoaDto = {
+        nome: 'Tatiana',
+        email: '',
+        password: '123456',
+      };
+
+      const response = await request(app.getHttpServer())
+        .post('/pessoas')
+        .send(createPessoaDto)
+        .expect(HttpStatus.BAD_REQUEST);
+      expect(response.body.message).toContain('email must be an email');
+    });
+
+    it('deve falhar se a senha for vazia', async () => {
+      const createPessoaDto = {
+        nome: 'Tatiana',
+        email: 'tatiana1@email.com',
+        password: '',
+      };
+
+      const response = await request(app.getHttpServer())
+        .post('/pessoas')
+        .send(createPessoaDto)
+        .expect(HttpStatus.BAD_REQUEST);
+      expect(response.body.message).toContain('password should not be empty');
+    });
+  });
+  describe('GET /pessoas', () => {
+    it('deve retornar todas as pessoas', async () => {
+      await request(app.getHttpServer())
+        .post('/pessoas')
+        .send({
+          email: 'tatiana@email.com',
+          nome: 'Tatiana',
+          password: '123456',
+        })
+        .expect(HttpStatus.CREATED);
+
+      const response = await request(app.getHttpServer())
+        .get('/pessoas')
+        .set('Authorization', `Bearer ${authToken}`)
+        .expect(HttpStatus.OK);
+
+      expect(response.body).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            id: expect.any(Number),
+            email: 'tatiana@email.com',
+            nome: 'Tatiana',
+          }),
+        ]),
+      );
+    });
+  });
+  describe('/pessoas/:id (GET)', () => {
+    it('deve falhar quando usuário não está logado', async () => {
+      const createPessoaDto = {
+        nome: 'Tatiana',
+        email: 'tatiana1@email.com',
+        password: '123456',
+      };
+      const pessoa = await request(app.getHttpServer())
+        .post('/pessoas')
+        .send(createPessoaDto)
+        .expect(HttpStatus.CREATED);
+
+      const response = await request(app.getHttpServer())
+        .get(`/pessoas/${pessoa.body.id}`)
+        .expect(HttpStatus.UNAUTHORIZED);
+
+      expect(response.body).toEqual({
+        error: 'Unauthorized',
+        message: 'Não logado!',
+        statusCode: 401,
+      });
+    });
+    it('deve retornar a Pessoa quando usuário está logado', async () => {
+      const createPessoaDto = {
+        nome: 'Tatiana',
+        email: 'tatiana1@email.com',
+        password: '123456',
+      };
+      const pessoaResponse = await request(app.getHttpServer())
+        .post('/pessoas')
+        .send(createPessoaDto)
+        .expect(HttpStatus.CREATED);
+      const token = authToken;
+
+      const response = await request(app.getHttpServer())
+        .get(`/pessoas/${pessoaResponse.body.id}`)
+        .set('Authorization', `Bearer ${token}`)
+        .expect(HttpStatus.OK);
+
+      expect(response.body).toEqual({
+        nome: createPessoaDto.nome,
+        email: createPessoaDto.email,
+        passwordHash: expect.any(String),
+        id: pessoaResponse.body.id,
+        createdAt: expect.any(String),
+        updatedAt: expect.any(String),
+        isActive: true,
+        picture: '',
+      });
+    });
+    it('deve retornar erro para pessoa não encontrada', async () => {
+      await request(app.getHttpServer())
+        .get('/pessoas/9999') // ID fictício
+        .set('Authorization', `Bearer ${authToken}`)
+        .expect(HttpStatus.NOT_FOUND);
+    });
+  });
+  describe('PATCH /pessoas/:id', () => {
+    it('deve atualizar uma pessoa', async () => {
+      const createResponse = await request(app.getHttpServer())
+        .post('/pessoas')
+        .send({
+          email: 'tatiana@email.com',
+          nome: 'Tatiana',
+          password: '123456',
+        })
+        .expect(HttpStatus.CREATED);
+
+      const personId = createResponse.body.id;
+
+      const authToken = await login(app, 'tatiana@email.com', '123456');
+
+      const updateResponse = await request(app.getHttpServer())
+        .patch(`/pessoas/${personId}`)
+        .send({
+          nome: 'Tatiana Atualizado',
+        })
+        .set('Authorization', `Bearer ${authToken}`)
+        .expect(HttpStatus.OK);
+
+      expect(updateResponse.body).toEqual(
+        expect.objectContaining({
+          id: personId,
+          nome: 'Tatiana Atualizado',
+        }),
+      );
+    });
+
+    it('deve retornar erro para pessoa não encontrada', async () => {
+      await request(app.getHttpServer())
+        .patch('/pessoas/9999') // ID fictício
+        .send({
+          nome: 'Nome Atualizado',
+        })
+        .set('Authorization', `Bearer ${authToken}`)
+        .expect(HttpStatus.NOT_FOUND);
+    });
+  });
+
+  describe('DELETE /pessoas/:id', () => {
+    it('deve remover uma pessoa', async () => {
+      const createResponse = await request(app.getHttpServer())
+        .post('/pessoas')
+        .send({
+          email: 'tatiana@email.com',
+          nome: 'Tatiana',
+          password: '123456',
+        })
+        .expect(HttpStatus.CREATED);
+
+      const authToken = await login(app, 'tatiana@email.com', '123456');
+
+      const personId = createResponse.body.id;
+
+      const response = await request(app.getHttpServer())
+        .delete(`/pessoas/${personId}`)
+        .set('Authorization', `Bearer ${authToken}`)
+        .expect(HttpStatus.OK);
+
+      expect(response.body.email).toBe('tatiana@email.com');
+    });
+
+    it('deve retornar erro para pessoa não encontrada', async () => {
+      await request(app.getHttpServer())
+        .delete('/pessoas/9999') // ID fictício
+        .set('Authorization', `Bearer ${authToken}`)
+        .expect(HttpStatus.NOT_FOUND);
     });
   });
 
