84a75469e0cd4fc52797f6ab4da0c08e3e8d9a8e
Tatiana Pacheco Barreto <tatianapacheco09@gmail.com>
2026-05-20 16:16:59 -0300

    test post pessoas

---
+++ src/app/config/app.config.ts
@@ -0,0 +1,13 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { ParseIntIdPipe } from 'src/common/pipes/parse-int-id.pipe';
+
+export default (app: INestApplication) => {
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // remove chaves que não estão no DTO
+      forbidNonWhitelisted: true, // retorna erro se houver chaves que não estão no DTO
+      transform: false, // tenta transformar os tipos de dados de parâmetros e DTOs, mas a performance não é boa
+    }),
+    new ParseIntIdPipe(),
+  );
+};
--- test/pessoas.e2e-spec.ts
+++ test/pessoas.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from '../src/app/app.module';
@@ -12,6 +12,9 @@ import globalConfig from 'src/global-config/global-config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import * as path from 'path';
+import { ParseIntIdPipe } from 'src/common/pipes/parse-int-id.pipe';
+import appConfig from 'src/app/config/app.config';
+import { create } from 'domain';
 
 describe('PessoasController (e2e)', () => {
   let app: INestApplication<App>;
@@ -33,6 +36,7 @@ describe('PessoasController (e2e)', () => {
           database: 'crud_nest_testing',
           autoLoadEntities: true, // carrega automaticamente as entidades do projeto sem especificá-las
           synchronize: true, // Sincroniza com o banco de dados. Não deve ser usado em produção
+          dropSchema: true, // apaga o banco de dados a cada teste
         }),
         RecadosModule,
         PessoasModule,
@@ -42,11 +46,101 @@ describe('PessoasController (e2e)', () => {
     }).compile();
 
     app = module.createNestApplication();
+    appConfig(app);
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    //
+  describe('/pessoas (POST)', () => {
+    it('deve criar uma nova pessoa com sucesso', async () => {
+      const createPessoaDto = {
+        nome: 'Tatiana',
+        email: 'tatiana1@email.com',
+        password: '123456',
+      };
+
+      const response = await request(app.getHttpServer())
+        .post('/pessoas')
+        .send(createPessoaDto)
+        .expect(HttpStatus.CREATED);
+      expect(response.body).toEqual({
+        nome: createPessoaDto.nome,
+        email: createPessoaDto.email,
+        passwordHash: expect.any(String),
+        id: expect.any(Number),
+        createdAt: expect.any(String),
+        updatedAt: expect.any(String),
+        isActive: true,
+        picture: '',
+      });
+      expect(response.body).toHaveProperty('id');
+      expect(response.body.nome).toBe(createPessoaDto.nome);
+      expect(response.body.email).toBe(createPessoaDto.email);
+    });
+
+    it('deve falhar se o email for duplicado', async () => {
+      const createPessoaDto = {
+        nome: 'Tatiana',
+        email: 'tatiana1@email.com',
+        password: '123456',
+      };
+
+      await request(app.getHttpServer())
+        .post('/pessoas')
+        .send(createPessoaDto)
+        .expect(HttpStatus.CREATED);
+
+      const response = await request(app.getHttpServer())
+        .post('/pessoas')
+        .send(createPessoaDto)
+        .expect(HttpStatus.CONFLICT);
+      expect(response.body.message).toBe('Email já cadastrado');
+    });
+
+    it('deve falhar se o nome for muito longo', async () => {
+      const createPessoaDto = {
+        nome: 'a'.repeat(101),
+        email: 'tatiana1@email.com',
+        password: '123456',
+      };
+
+      const response = await request(app.getHttpServer())
+        .post('/pessoas')
+        .send(createPessoaDto)
+        .expect(HttpStatus.BAD_REQUEST);
+      expect(response.body.message).toContain(
+        'nome must be shorter than or equal to 100 characters',
+      );
+    });
+
+    it('deve falhar se o email for inválido', async () => {
+      const createPessoaDto = {
+        nome: 'Tatiana',
+        email: 'email-invalido',
+        password: '123456',
+      };
+
+      const response = await request(app.getHttpServer())
+        .post('/pessoas')
+        .send(createPessoaDto)
+        .expect(HttpStatus.BAD_REQUEST);
+      expect(response.body.message).toContain('email must be an email');
+    });
+
+    it('deve falhar se a senha for muito curta', async () => {
+      const createPessoaDto = {
+        nome: 'Tatiana',
+        email: 'tatiana1@email.com',
+        password: '123',
+      };
+
+      const response = await request(app.getHttpServer())
+        .post('/pessoas')
+        .send(createPessoaDto)
+        .expect(HttpStatus.BAD_REQUEST);
+      expect(response.body.message).toContain(
+        'password must be longer than or equal to 5 characters',
+      );
+    });
   });
 
   afterEach(async () => {
