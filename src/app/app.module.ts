ef8baa55a7358cbf57f550d99c949df05e2899a9
Tatiana Pacheco Barreto <tatianapacheco09@gmail.com>
2026-05-21 10:17:41 -0300

    config de segurança

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,18 @@ import { GlobalConfigModule } from 'src/global-config/global-config.module';
 import { AuthModule } from 'src/auth/auth.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import * as path from 'path';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 // TypeOrmModule.forRoot() -> para raiz da aplicação
 @Module({
   imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 60,
+        blockDuration: 5000,
+      },
+    ]),
     ServeStaticModule.forRoot({
       rootPath: path.resolve(__dirname, '..', '..', 'pictures'),
       serveRoot: '/pictures',
@@ -42,6 +51,12 @@ import * as path from 'path';
     AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,22 @@ import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ParseIntIdPipe } from './common/pipes/parse-int-id.pipe';
 import appConfig from './app/config/app.config';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   appConfig(app);
-  await app.listen(process.env.PORT ?? 3000);
+
+  if (process.env.NODE_ENV === 'production') {
+    // helmet -> cabeçalho de segurança no protocolo HTTP
+    app.use(helmet());
+
+    // cors -> permitir que outro domínio faça requests na nossa API
+    app.enableCors({
+      origin: 'https://meuapp.com.br',
+    });
+  }
+
+  await app.listen(process.env.APP_PORT ?? 3000);
 }
 bootstrap();
